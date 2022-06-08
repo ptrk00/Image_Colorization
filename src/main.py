7762cf82e510d4e31f3d2a0a10f567b824ced3a6
ptrk00 <76401343+Piotr12309@users.noreply.github.com>
2022-06-08 22:33:29 +0200

    Add v1 sample images

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import os
 
 import numpy as np
 from keras.engine.input_layer import InputLayer
-from keras.layers import Conv2D, UpSampling2D
+from keras.layers import Conv2D, UpSampling2D, LayerNormalization, Conv2DTranspose
 from keras.preprocessing.image import img_to_array, load_img
 from skimage.transform import resize
 from skimage.color import rgb2lab, lab2rgb
@@ -10,6 +10,8 @@ from keras.models import Sequential
 from skimage.io import imsave
 
 from src.graphs import generate_graph
+
+from keras.preprocessing.image import ImageDataGenerator
 
 img_width = 128
 img_height = 128
@@ -65,25 +67,52 @@ def build_model():
     return model
 
 
-def build_model_2():
+def build_model_eccv():
     model = Sequential()
+
     model.add(InputLayer(input_shape=(img_width, img_height, 1)))
     model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
-    model.add(Conv2D(64, (3, 3), activation='relu', padding='same', strides=2))
+    model.add(Conv2D(64, (3, 3), activation='relu', strides=2, padding='same'))
+    model.add(LayerNormalization())
+
     model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
-    model.add(Conv2D(128, (3, 3), activation='relu', padding='same', strides=2))
+    model.add(Conv2D(128, (3, 3), activation='relu', strides=2, padding='same'))
+    model.add(LayerNormalization())
+
+    model.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
     model.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
     model.add(Conv2D(256, (3, 3), activation='relu', padding='same', strides=2))
+    model.add(LayerNormalization())
+
     model.add(Conv2D(512, (3, 3), activation='relu', padding='same'))
+    model.add(Conv2D(512, (3, 3), activation='relu', padding='same'))
+    model.add(Conv2D(512, (3, 3), activation='relu', padding='same'))
+    model.add(LayerNormalization())
+
+    model.add(Conv2D(512, (3, 3), activation='relu', padding='same', dilation_rate=2))
+    model.add(Conv2D(512, (3, 3), activation='relu', padding='same', dilation_rate=2))
+    model.add(Conv2D(512, (3, 3), activation='relu', padding='same', dilation_rate=2))
+    model.add(LayerNormalization())
+
+    model.add(Conv2D(512, (3, 3), activation='relu', padding='same', dilation_rate=2))
+    model.add(Conv2D(512, (3, 3), activation='relu', padding='same', dilation_rate=2))
+    model.add(Conv2D(512, (3, 3), activation='relu', padding='same', dilation_rate=2))
+    model.add(LayerNormalization())
+
+    model.add(Conv2D(512, (3, 3), activation='relu', padding='same', dilation_rate=2))
+    model.add(Conv2D(512, (3, 3), activation='relu', padding='same', dilation_rate=2))
+    model.add(Conv2D(512, (3, 3), activation='relu', padding='same', dilation_rate=2))
+    model.add(LayerNormalization())
+
+    model.add(Conv2DTranspose(256, kernel_size=4, strides=2, padding='same', activation='relu'))
     model.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
-    model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
-    model.add(UpSampling2D((2, 2)))
-    model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
-    model.add(UpSampling2D((2, 2)))
-    model.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
-    model.add(Conv2D(2, (3, 3), activation='tanh', padding='same'))
-    model.add(UpSampling2D((2, 2)))
-    model.compile(optimizer='rmsprop', loss='mse')
+    model.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
+    model.add(Conv2D(313, (1, 1), padding='same'))
+    model.add(Conv2D(2, (1, 1), padding='same', dilation_rate=1))
+    model.add(UpSampling2D((4, 4), interpolation='bilinear'))
+
+    model.summary()
+    model.compile(optimizer='rmsprop', loss='mse', metrics=['acc'])
     return model
 
 
@@ -141,11 +170,33 @@ def predict(model, x_train):
     return model.predict(x_train) * 128
 
 
+transformedImgGenerator = ImageDataGenerator(
+    shear_range=0.2,
+    zoom_range=0.2,
+    rotation_range=20,
+    horizontal_flip=True
+)
+
+
+def imageGenerator(X, Y):
+    for data in transformedImgGenerator.flow(X, Y, batch_size=20):
+        yield data
+
+
 def run(batch_size, epochs):
-    model = build_model()
+    model = build_model_eccv()
     x, y = generate_train_data()
 
-    history = model.fit(x=x, y=y, batch_size=batch_size, epochs=epochs, validation_split=0.2)
+    slicePos = int(0.8 * len(x))
+    x_train = x[:slicePos]
+    y_train = y[:slicePos]
+
+    x_validate = x[slicePos:]
+    y_validate = y[slicePos:]
+
+    # history = model.fit(x=x, y=y, batch_size=batch_size, epochs=epochs, validation_split=0.2)
+    history = model.fit(imageGenerator(x_train, y_train), validation_data=imageGenerator(x_validate, y_validate),
+                        steps_per_epoch=84, validation_steps=21, epochs=epochs)
     print(model.evaluate(x, y, batch_size=batch_size))
     x_train = load_test_data()
 
@@ -156,4 +207,5 @@ def run(batch_size, epochs):
                    history.history['val_loss'])
 
 
-run(20, 30)
+if __name__ == '__main__':
+    run(20, 10)
